e5fc68a0fe26447a6cf1ef3b80bb4f0947a8e3cb
Kaushik Bhat <kaushik.bantval98@gmail.com>
2020-04-16 18:33:23 +0530

    jQuery & bootstrap

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-// Invoking tooltip
+// Document ready function
+
 $(function () {
+    // Invoking tooltip
     $('[data-toggle="tooltip"]').tooltip();
+
+    // Carousel interval 2s
+    $('#mycarousel').carousel({
+        Interval: 1000
+    });
+
+    // Carousel pause
+    $('#carousel-pause').click(function () {
+        $('#mycarousel').carousel('pause');
+    });
+
+    // Carousel resume
+    $('#carousel-play').click(function () {
+        $('#mycarousel').carousel('cycle');
+    });
 });
