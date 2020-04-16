8b4c012ed8beb2d48760c8814efb211cfac34a24
Kaushik Bhat <kaushik.bantval98@gmail.com>
2020-04-16 18:45:27 +0530

    add jQuery

--- js/script.js
+++ js/script.js
@@ -6,16 +6,19 @@ $(function () {
 
     // Carousel interval 2s
     $('#mycarousel').carousel({
-        Interval: 1000
+        interval: 1000
     });
 
-    // Carousel pause
-    $('#carousel-pause').click(function () {
-        $('#mycarousel').carousel('pause');
-    });
-
-    // Carousel resume
-    $('#carousel-play').click(function () {
-        $('#mycarousel').carousel('cycle');
+    // Carousel pause/resume
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else  {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
     });
 });
